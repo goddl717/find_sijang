2c002b9f1411a954f1f4d03c156d943f753886b2
JIWON <jwkim9205@gmail.com>
2020-08-12 16:17:08 +0900

    MainActivity 변경

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,139 +1,96 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    //define view objects
-    private DatabaseReference mPostReference;
-    EditText editTextEmail;
-    EditText editTextPassword;
-    EditText editTextName;
-    EditText editTextTel;
-    EditText editAddress;
-    CheckBox checkBoxAdmin;
-    Button buttonSignup;
-    TextView textviewSingin;
-    TextView textviewMessage;
-    ProgressDialog progressDialog;
-    //define firebase object
-    FirebaseAuth firebaseAuth;
+    private static final String TAG = "ProfileActivity";
+
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+    private TextView textivewDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
 
-        //initializig firebase auth object
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        if (firebaseAuth.getCurrentUser() != null) {
-            //이미 로그인 되었다면 이 액티비티를 종료함
-            finish();
-            //그리고 profile 액티비티를 연다.
-            startActivity(new Intent(getApplicationContext(), ProfileActivity.class)); //추가해 줄 ProfileActivity
-        }
         //initializing views
-        editTextName = (EditText) findViewById(R.id.editTextName);
-        editTextTel = (EditText) findViewById(R.id.editTextTel);
-        editAddress = (EditText) findViewById(R.id.editTexAdress);
-        checkBoxAdmin = (CheckBox)findViewById(R.id.isAdmin);
-        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        textviewSingin = (TextView) findViewById(R.id.textViewSignin);
-        textviewMessage = (TextView) findViewById(R.id.textviewMessage);
-        buttonSignup = (Button) findViewById(R.id.buttonSignup);
-        progressDialog = new ProgressDialog(this);
+        textViewUserEmail = (TextView) findViewById(R.id.textviewUserEmail);
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        textivewDelete = (TextView) findViewById(R.id.textviewDelete);
 
-        //button click event
-        buttonSignup.setOnClickListener(this);
-        textviewSingin.setOnClickListener(this);
-    }
-
-    //Firebse creating a new user
-    private void registerUser() {
-        //사용자가 입력하는 email, password를 가져온다.
-        final String name = editTextName.getText().toString().trim();
-        final String tel = editTextTel.getText().toString().trim();
-        final String address = editAddress.getText().toString().trim();
-        final String email = editTextEmail.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-        //email과 password가 비었는지 아닌지를 체크 한다.
-        if (TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+        //유저가 로그인 하지 않은 상태라면 null 상태이고 이 액티비티를 종료하고 로그인 액티비티를 연다.
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
         }
 
-        //email과 password가 제대로 입력되어 있다면 계속 진행된다.
-        progressDialog.setMessage("등록중입니다. 기다려 주세요...");
-        progressDialog.show();
+        //유저가 있다면, null이 아니면 계속 진행
+        FirebaseUser user = firebaseAuth.getCurrentUser();
 
-        //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                            postFirebaseDatabase(user.getUid(), name, tel, email, address, checkBoxAdmin.isChecked());
-                            finish();
-                            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
-                        } else {
-                            //에러발생시
-                            textviewMessage.setText("에러유형\n - 이미 등록된 이메일  \n -암호 최소 6자리 이상 \n - 서버에러");
-                            Toast.makeText(MainActivity.this, "등록 에러!", Toast.LENGTH_SHORT).show();
-                        }
-                        progressDialog.dismiss();
-                    }
-                });
+        //textViewUserEmail의 내용을 변경해 준다.
+        textViewUserEmail.setText("반갑습니다.\n" + user.getEmail() + "으로 로그인 하였습니다.");
+
+        //logout button event
+        buttonLogout.setOnClickListener(this);
+        textivewDelete.setOnClickListener(this);
 
     }
-
-    public void postFirebaseDatabase(String uid, String name, String tel, String email, String address, boolean isAdmin) {
-        mPostReference = FirebaseDatabase.getInstance().getReference();
-        UserPost post = new UserPost(name, tel, email, address, isAdmin);
-        Map<String, Object> postValues = post.toMap();
-        Map<String, Object> childUpdates = new HashMap<>();
-        childUpdates.put("/users/" + uid, postValues);
-        mPostReference.updateChildren(childUpdates);
-    }
-
 
     @Override
     public void onClick(View view) {
-        if (view == buttonSignup) {
-            //TODO
-            registerUser();
+        if (view == buttonLogout) {
+            firebaseAuth.signOut();
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
         }
-
-        if (view == textviewSingin) {
-            //TODO
-            startActivity(new Intent(this, LoginActivity.class)); //추가해 줄 로그인 액티비티
+        //회원탈퇴를 클릭하면 회원정보를 삭제한다. 삭제전에 컨펌창을 하나 띄워야 겠다.
+        if (view == textivewDelete) {
+            AlertDialog.Builder alert_confirm = new AlertDialog.Builder(MainActivity.this);
+            alert_confirm.setMessage("정말 계정을 삭제 할까요?").setCancelable(false).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            user.delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            Toast.makeText(MainActivity.this, "계정이 삭제 되었습니다.", Toast.LENGTH_LONG).show();
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+                                        }
+                                    });
+                        }
+                    }
+            );
+            alert_confirm.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MainActivity.this, "취소", Toast.LENGTH_LONG).show();
+                }
+            });
+            alert_confirm.show();
         }
     }
 }
