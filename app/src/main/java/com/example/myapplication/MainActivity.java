f9de406c3a61ba8a682ee368029e3f8ea2c9fee7
goddl717 <37294326+goddl717@users.noreply.github.com>
2020-08-16 00:37:22 +0900

    sd
    sd

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,14 +10,23 @@ import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity  {
     private static final String TAG = "MainActivity";
     //확인
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        myRef.setValue("Hello, World!");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActionBar actionBar = getSupportActionBar();
--- app/src/main/java/com/example/myapplication/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/myapplication/ui/search/SearchFragment.java
@@ -8,11 +8,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.myapplication.R;
+import com.example.myapplication.UserPost;
+import com.example.myapplication.users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SearchFragment extends Fragment {
 
@@ -21,6 +32,10 @@ public class SearchFragment extends Fragment {
     public static SearchFragment newInstance() {
         return new SearchFragment();
     }
+
+    private DatabaseReference mDatabase;
+
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -31,7 +46,31 @@ public class SearchFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
+
+
+
+        ValueEventListener postListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                //users post = dataSnapshot.getValue(users.class);
+
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                Log.d("tag", "Value is: " + map);
+                //Log.v("jiwon",post.getEmail().toString());
+                // ...
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("jiwon", "loadPost:onCancelled", databaseError.toException());
+                // ...
+            }
+        };
+        mDatabase.addValueEventListener(postListener);
         // TODO: Use the ViewModel
     }
 
