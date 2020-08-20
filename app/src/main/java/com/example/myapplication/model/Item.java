6ea2cda6b79e092c312b49539b1c807277274328
JIWON <jwkim9205@gmail.com>
2020-08-20 19:01:30 +0900

    ee

--- app/src/main/java/com/example/myapplication/model/Item.java
+++ app/src/main/java/com/example/myapplication/model/Item.java
@@ -2,4 +2,49 @@ package com.example.myapplication.model;
 
 public class Item {
     private String itemName;
+    private String imageUrl;
+    private String count;
+    private String price;
+
+    public Item() {
+    }
+
+    public Item(String itemName, String imageUrl, String count, String price) {
+        this.itemName = itemName;
+        this.imageUrl = imageUrl;
+        this.count = count;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
--- app/src/main/java/com/example/myapplication/model/Store.java
+++ app/src/main/java/com/example/myapplication/model/Store.java
@@ -1,31 +1,81 @@
 package com.example.myapplication.model;
 
-import com.example.myapplication.model.Item;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Store {
     private String userId;
     private String storeName;
     private String imageUrl;
     private String detail;
-    private Item item;
+    private String category;
+    HashMap<String, Item> items = new HashMap<>();
 
-    public Store(String userId, String storeName, String imageUrl, String detail, Item item) {
+    public HashMap<String, Item> getItems() {
+        return items;
+    }
+
+    public void setItems(HashMap<String, Item> items) {
+        this.items = items;
+    }
+
+    public Store(String userId, String storeName, String imageUrl, String detail, String category) {
         this.userId = userId;
         this.storeName = storeName;
         this.imageUrl = imageUrl;
         this.detail = detail;
-        this.item = item;
+        this.category = category;
     }
-    public Map<String, Object> toMap() {
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("userId", userId);
-        result.put("storeName", storeName);
-        result.put("imageUrl", imageUrl);
-        result.put("detail", detail);
-        result.put("item", item);
-        return result;
+
+    public Store() {
     }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+//    public Map<String, Object> toMap() {
+//        HashMap<String, Object> result = new HashMap<>();
+//        result.put("userId", userId);
+//        result.put("storeName", storeName);
+//        result.put("imageUrl", imageUrl);
+//        result.put("detail", detail);
+//        result.put("item", item.toMap());
+//        return result;
+//    }
 }
